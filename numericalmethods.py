181da53542ebe36b22b7f4f294fb1d3f71b80b71
Robert Stein <robert.david.stein@hotmail.com>
2016-10-26 23:11:00 +0100

    Backup 25/10/16

--- numericalmethods.py
+++ numericalmethods.py
@@ -1,13 +1,32 @@
 
 
-def euler(d2ydx2,yi, olddydx, h):
+def expliciteuler(df1,yi, olddydx, h):
 	yf = yi + (olddydx*h)
-	dydx =  olddydx + (h*d2ydx2(yi))
+	dydx =  olddydx + df1(yi)
 	return  yf, dydx
 	
-def leapfrog(d2ydx2, yi, olddydx,  h):
-	yf = yi + (olddydx*h) + (0.5 * d2ydx2(yi)*(h**2))
-	a0 = d2ydx2(yi)
-	a1 = d2ydx2(yf)
+def leapfrog(df2, yi, olddydx,  h):
+	yf = yi + (olddydx*h) + (0.5 * df2(yi)*(h**2))
+	a0 = df2(yi)
+	a1 = df2(yf)
 	dydx = olddydx + 0.5*(a0+a1)*h
 	return  yf, dydx
+	
+def rk4(df1, yi, olddydx,  h):
+	k1 = df1(yi)
+	k2 = df1(yi + k1*h/2)
+	k3 = df1(yi + k2*h/2)
+	k4 = df1(yi + k3*h)
+	dydx = (k1 + 2*(k2+k3) + k4)/6
+	yf = yi + (h*dydx)
+	print yi, yf, k1, k2, k3, k4, dydx
+	return  yf, dydx
+	
+def impliciteuler(df1,yi, olddydx, h):
+	guessdydx =  olddydx + df1(yi)
+	guessy = yi + (guessdydx*h)
+	dydx = olddydx + df1(guessy)
+	yf = yi + (dydx*h)
+	return  yf, dydx
+	
+
--- singlependulum.py
+++ singlependulum.py
@@ -3,25 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numericalmethods as nm
 
-h=0.1
+h=0.02
 omega = 2 * np.pi
-trange = np.arange(0, 20, h)
+trange = np.arange(0, 8, h)
 
 def f(t):
 	return np.cos(omega*t)
+	
+def df1(y):
+	d1 = df2(y) * h
+	#d1 = - omega * y
+	return d1
 
-def d2ydx2(y):
-	return -(omega**2)* y
+def df2(y):
+	return (-omega**2)* y
 	
 truey = f(trange)
-fity=[1.]
+fity=[1.,]
 fitdydx=[0.]
 
 for i in range(1, trange.size):
 	oldy = fity[i-1]
 	olddydx = fitdydx[i-1]
 	
-	fit, dydx = nm.leapfrog(d2ydx2, oldy, olddydx, h)
+	fit, dydx = nm.impliciteuler(df1, oldy, olddydx, h)
 	
 	fity.append(fit)
 	fitdydx.append(dydx)
